8f6d21d3c67895e20568061d9f36dfa4391176e6
kawsarahmed-012 <mdkowsir369@gmail.com>
2023-12-27 02:23:11 +0600

    adding default value for completed field

--- src/db/models/todo.ts
+++ src/db/models/todo.ts
@@ -6,13 +6,8 @@ export interface TodoInput {
 }
 
 const todoSchema = new mongoose.Schema<TodoInput>({
-  title: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 128,
-  },
-  completed: Boolean,
+  title: { type: String, required: true, minlength: 2, maxlength: 128 },
+  completed: { type: Boolean, default: false },
 });
 
 export const Todo = (mongoose.models["Todo"] ||

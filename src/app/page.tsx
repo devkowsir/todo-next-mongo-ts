b5bce663c7484b8ada5c351864a256c73763911e
kawsarahmed-012 <mdkowsir369@gmail.com>
2023-12-26 23:19:35 +0600

    fetch todos from db and render todos component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,8 @@
-export default function Home() {}
+import { Todos } from "@/components/todo/todos";
+import { getTodos } from "@/db/controllers/todo";
+
+export default async function Home() {
+  const todos = await getTodos();
+
+  return <Todos initialTodos={todos} />;
+}

24f09b12ec12d6219f6051ebafb4e15f3c8008fc
kawsarahmed-012 <mdkowsir369@gmail.com>
2023-12-26 22:56:16 +0600

    adding basic todo model

---
+++ src/db/models/todo.ts
@@ -0,0 +1,19 @@
+import mongoose, { Model } from "mongoose";
+
+export interface TodoInput {
+  title: string;
+  completed?: boolean;
+}
+
+const todoSchema = new mongoose.Schema<TodoInput>({
+  title: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 128,
+  },
+  completed: Boolean,
+});
+
+export const Todo = (mongoose.models["Todo"] ||
+  mongoose.model<TodoInput>("Todo", todoSchema, "Todo")) as Model<TodoInput>;

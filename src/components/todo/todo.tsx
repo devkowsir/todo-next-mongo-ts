b759281e4c3863ba0baf28b2cf417f7e0287ba2c
kawsarahmed-012 <mdkowsir369@gmail.com>
2023-12-27 02:24:07 +0600

    frontend part of toggling completion state is done

--- src/components/todo/todo.tsx
+++ src/components/todo/todo.tsx
@@ -1,7 +1,37 @@
+import { Dispatch, SetStateAction, useCallback } from "react";
+
 interface TodoProps {
   todo: ITodo;
+  setTodos: Dispatch<SetStateAction<ITodo[]>>;
 }
 
-export const Todo = ({ todo }: TodoProps) => {
-  return <li className="flex gap-2">{todo.title}</li>;
+export const Todo = ({
+  todo: { _id, completed, title },
+  setTodos,
+}: TodoProps) => {
+  const handleToggleCompletion = useCallback(async () => {
+    const toggleCompletionState = () => {
+      setTodos((todos) =>
+        todos.map((todo) =>
+          todo._id === _id ? { ...todo, completed: !todo.completed } : todo
+        )
+      );
+    };
+
+    toggleCompletionState();
+  }, [setTodos]);
+
+  return (
+    <li className="flex gap-2">
+      <input
+        className="cursor-pointer"
+        type="checkbox"
+        checked={completed}
+        onChange={handleToggleCompletion}
+      />
+      <div className="cursor-pointer" onClick={handleToggleCompletion}>
+        {title}
+      </div>
+    </li>
+  );
 };

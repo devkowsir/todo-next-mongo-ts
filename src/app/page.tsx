cc8c925a11c835b02a1e9ff9227d353210e53cc7
kawsarahmed-012 <mdkowsir369@gmail.com>
2023-12-27 13:13:23 +0600

    fixing production mode caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Todos } from "@/components/todo/todos";
 import { getTodos } from "@/db/controllers/todo";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const todos = await getTodos();
 

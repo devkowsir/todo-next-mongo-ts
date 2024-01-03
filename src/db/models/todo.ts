a9539875b64f5fdada51ae3fef1be47b2d690c15
kawsarahmed-012 <mdkowsir369@gmail.com>
2024-01-03 13:36:16 +0600

    connected user model with todo model

--- src/db/models/todo.ts
+++ src/db/models/todo.ts
@@ -1,14 +1,16 @@
-import mongoose, { Model } from "mongoose";
+import { Model, Schema, SchemaTypes, Types, model, models } from "mongoose";
 
 export interface TodoInput {
   title: string;
+  user: Types.ObjectId;
   completed?: boolean;
 }
 
-const todoSchema = new mongoose.Schema<TodoInput>({
+const todoSchema = new Schema<TodoInput>({
   title: { type: String, required: true, minlength: 2, maxlength: 128 },
+  user: { type: SchemaTypes.ObjectId, ref: "User" },
   completed: { type: Boolean, default: false },
 });
 
-export const Todo = (mongoose.models["Todo"] ||
-  mongoose.model<TodoInput>("Todo", todoSchema, "Todo")) as Model<TodoInput>;
+export const Todo = (models["Todo"] ||
+  model<TodoInput>("Todo", todoSchema, "Todo")) as Model<TodoInput>;

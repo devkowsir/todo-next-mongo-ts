1434a39533187cd4477ceada781bfc537090cf60
kawsarahmed-012 <mdkowsir369@gmail.com>
2024-01-03 13:35:52 +0600

    added user model

---
+++ src/db/models/user.ts
@@ -0,0 +1,47 @@
+import { Model, Schema, SchemaTypes, Types, model, models } from "mongoose";
+
+export interface UserInput {
+  name: string;
+  email: string;
+  password: string;
+  todos?: Types.ObjectId[];
+  isVerified?: boolean;
+  isAdmin?: boolean;
+  verifyToken?: string;
+  verifyTokenExpiry?: Date;
+  forgotPasswordToken?: string;
+  forgotPasswordExpiry?: Date;
+}
+
+const userSchema = new Schema<UserInput>({
+  name: {
+    type: String,
+    required: true,
+    minlength: [3, "Name must be at least 3 character long."],
+    maxlength: [30, "Name cannot be more than 30 characters long"],
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [
+      new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+      "Provide a valid email address",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Password must be at least 6 characters long"],
+  },
+  todos: { type: [SchemaTypes.ObjectId], default: [], ref: "Todo" },
+  isVerified: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false },
+  verifyToken: { type: String },
+  verifyTokenExpiry: { type: Date },
+  forgotPasswordToken: { type: String },
+  forgotPasswordExpiry: { type: Date },
+});
+
+export const User = (models["User"] ||
+  model<UserInput>("User", userSchema, "User")) as Model<UserInput>;

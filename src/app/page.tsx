0f6259f98e0e7c8e1974ec60e1683559e32e9ae6
kawsarahmed-012 <mdkowsir369@gmail.com>
2024-01-09 20:31:38 +0600

    authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Todos } from "@/components/todo/todos";
 import { getTodos } from "@/db/controllers/todo";
+import getAuthInfo from "@/utils/get-auth-session";
+import { notFound } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const todos = await getTodos();
+  const user = getAuthInfo();
+  if (!user) return notFound();
+  const todos = await getTodos({ user: user.id });
 
   return <Todos initialTodos={todos} />;
 }

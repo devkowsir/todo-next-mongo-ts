e12801a2ad0df7e8a1e44afe4a8669c8296a99df
kawsarahmed-012 <mdkowsir369@gmail.com>
2024-01-23 23:08:42 +0600

    failed to verify user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { Todos } from "@/components/todo/todos";
 import { findAllTodos } from "@/db/services";
 import getAuthInfo from "@/utils/get-auth-session";
-import { notFound } from "next/navigation";
+import { RedirectType, redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const user = getAuthInfo();
-  if (!user) return notFound();
+  if (!user) return redirect("/user/login", RedirectType.replace);
   const todos = await findAllTodos({ user: user.id });
 
   return <Todos initialTodos={todos} />;
